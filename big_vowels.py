7c3a33dcdc98ec62acd284444c8269fa8f8b81d4
ProgrammerAngelo <angelovpangilagan@gmail.com>
2024-10-13 08:59:37 -0400

    Final Result

--- big_vowels.py
+++ big_vowels.py
@@ -1,2 +1,13 @@
 #method for making the vowels upper case
+print("Making the vowels to upper")
+def vowelsToUpper(string):
+
+    vowels = "aeiou"
+
+    modified = ''.join([char.upper() if char.lower() in vowels  else char for char in string])
+
+    return modified
 #for displaying the result
+print( '" "==" "')
+print('"Hello world" == ',vowelsToUpper('"Hello world"'))
+print('"hello hi bye" == ',vowelsToUpper('"hello hi bye"'))
